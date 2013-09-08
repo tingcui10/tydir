fb101fd63466f0d973f492fd16ee2207b11bc12b
Ting Cui <ting.cui10@gmail.com>
2013-09-08 14:38:05 -0400

    Finance page update

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -8,9 +8,37 @@ module StaticPagesHelper
     youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
     youtube_id = $5
 end
-    %Q{<iframe title="YouTube video player" width="90%" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+    %Q{<iframe title="YouTube video player" width="80%" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+
 
 
 end
+
+ def youtube_embed_small(youtube_url)
+  if youtube_url[/youtu\.be\/([^\?]*)/]
+    youtube_id = $1
+  else
+    # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+    youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+    youtube_id = $5
+end
+ 
+	%Q{<iframe title="YouTube video player" width="80%" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+
+end
+
+def youtube_embed_wide(youtube_url)
+  if youtube_url[/youtu\.be\/([^\?]*)/]
+    youtube_id = $1
+  else
+    # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+    youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+    youtube_id = $5
+end
+ 
+	%Q{<iframe title="YouTube video player" width="100%" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+
+end
+
 
 end
